d35c4b21ad6a0c73afdd24c0f02266a54f83187a
johnson <24johnsonl0556@gmail.com>
2026-09-22 16:00:03 -0700

    first commit

---
+++ weatherviewer/src/test/java/canadacityviewer/com/weatherviewer/WeatherviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package canadacityviewer.com.weatherviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
